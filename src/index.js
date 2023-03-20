8c74c19e3f9639c54fc0ad7760647df755c928fc
Veronica <veronica.stevania@gmail.com>
2023-03-20 13:00:51 +0100

    Integrate Redux Persist to save employees' datas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store'
 //import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
 //import { reducer as formReducer } from 'redux-form'
 import './index.css'
 
@@ -14,17 +14,16 @@ import './index.css'
 
 // const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   //<React.StrictMode>    
-     //  <App />
-   //</React.StrictMode>
-  <Provider store={store}> 
-      <App />  
-  </Provider>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render (
+  <React.StrictMode>    
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+              <App />
+          </PersistGate>
+              
+      </Provider>
+  </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,26 @@
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import thunk from 'redux-thunk'
 import { configureStore } from "@reduxjs/toolkit"
 import { combineReducers } from 'redux'
 import employeeReducer from '../redux/slices/employeeSlice'
 import { reducer as formReducer } from 'redux-form'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 const reducer = combineReducers({
     form: formReducer, // mounted under "form"
-    employeeReducer : employeeReducer
+    employeeReducer : employeeReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 export const store = configureStore ({
-    reducer: reducer,
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
 })
+
+export const persistor = persistStore(store)
