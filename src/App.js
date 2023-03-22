223a7c36ba8d95e2fff774b0d85b5a8711b8fc6c
Veronica <veronica.stevania@gmail.com>
2023-03-22 19:02:48 +0100

    Use React lazy and Suspense to improving the app's performance

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-
-import CreateEmployee from './pages/CreateEmployee/CreateEmployee'
-import CurrentEmployees from './pages/CurrentEmployees/CurrentEmployees'
-import Error404 from './pages/Error404/Error404'
 
 import './App.css'
 
 /** 
- * @file App.js is the root file for this app. <br>
- * * @author Veronica BERNARD
- * @see <a href="https://github.com/VSABernard">My GitHub</a>
+ * Suspense is a React feature that allows for components to be loaded asynchronously. 
+ * React.lazy is a powerful feature of React that allows components to be loaded lazily from the server. 
  */
+
+const CreateEmployee = React.lazy(() => import('./pages/CreateEmployee/CreateEmployee'))
+const CurrentEmployees = React.lazy(() => import('./pages/CurrentEmployees/CurrentEmployees'))
+const Error404 = React.lazy(() => import('./pages/Error404/Error404'))
 
 const App= () => {
   return (
     <BrowserRouter>
+      <Suspense>
         <Routes>
           <Route path="/" element={<CreateEmployee/>} />
           <Route path="/CurrentEmployees" element={<CurrentEmployees/>} />    
           <Route path="*" element={<Error404/>} />
         </Routes>
+      </Suspense>  
     </BrowserRouter>    
   )
 }
